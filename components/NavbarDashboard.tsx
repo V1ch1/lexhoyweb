57d77cebeb1900a97906f736c09dc6765f8d3e25
V1ch1 <blancocasal@hotmail.com>
2025-09-22 10:46:45 +0200

    fix: Remove unused useState import from NavbarDashboard component

--- components/NavbarDashboard.tsx
+++ components/NavbarDashboard.tsx
@@ -1,7 +1,6 @@
 // components/NavbarDashboard.tsx
 "use client";
 
-import { useState } from "react";
 import { UserCircleIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 
